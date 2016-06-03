7571677e648f31e4ce7973f0f81e877ea43fb061
François Gindraud <francois.gindraud@gmail.com>
2016-06-03 14:21:29 +0200

    Travis test again.

--- build/linux/test.sh
+++ build/linux/test.sh
@@ -1,27 +1,30 @@
 #!/usr/bin/env bash
-set -xue
+#set -xue
 
 # FIXME
 # Avahi daemon is available on Travis linux environment.
 # Avahi is available on Travis machines, but doesn't seem to work (instances don't find each other).
 #
 # I know that is doesn't browse event localhost services if only loopback is available.
-# But eth0 exists and is up !
-# Checking settings and ip of eth0
+# eth0 is up, has an address, and avahi/nsswitch config seems ok...
+# Test with avahi tools...
 ip link
 ip addr
 cat /etc/avahi/avahi-daemon.conf
 cat /etc/nsswitch.conf
 
+avahi-publish -s blah _localshare._tcp 42042 &
+sleep 5s
+avahi-browse -tva
+killall avahi-publish
+
 ./localshare --version
 
-if false; then
-	# test transfer of dummy file
-	head -c 100000 /dev/urandom > data
-	./localshare -q -u data -p TEST &
-	./localshare -v --yes -d -t test -n TEST
-	wait
-	cmp data test/data
-fi
+# test transfer of dummy file
+head -c 100000 /dev/urandom > data
+./localshare -q -u data -p TEST &
+./localshare -v --yes -d -t test -n TEST
+wait
+cmp data test/data
 
-set +xue
+#set +xue
